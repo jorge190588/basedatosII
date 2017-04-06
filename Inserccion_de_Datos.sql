40a91006b0bca61673f8d79b4d7a8c1d213b47f1
EbarriosCode <guayoswing@gmail.com>
2017-04-06 12:56:56 -0600

    Script sql para la insercción de datos en la base de datos, Nota: utilizar este script para cuando se inserten datos a las diferentes tablas de está manera garantizamos tener la misma cantidad de datos o por lo menos una infinidad de datos en nuestras bases de datos locales

---
+++ Inserccion_de_Datos.sql
@@ -0,0 +1,10 @@
+use ComputerDB
+select * from Municipio
+select * from Departamento
+select * from Pais
+
+-- Volcado de datos Tabla Pais
+insert into Pais(nombrePais)values('Guatemala'),('México'),('USA'),('Argentina'),('Brasil')
+
+-- Volcado de datos Tabla Departamento
+insert into Departamento(nombreDepartamento,idPais) values(''
