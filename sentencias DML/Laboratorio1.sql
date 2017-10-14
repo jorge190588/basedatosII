4f64c1abce87e50871926b1cf9fa610ca96f05a5
jorgesantosn <netneill@hotmail.com>
2017-10-14 09:24:38 -0600

    scenario number 2

--- sentencias DML/Laboratorio1.sql
+++ sentencias DML/Laboratorio1.sql
@@ -8,3 +8,19 @@ where datepart(qq,s.fecha) in (1,2)
 and datename(dw,s.fecha)='Lunes'
 order by nombreCliente
 
+/*
+2.	Scenario: Reporte de artículos
+Given: El dueño del negocio requiere un listado de productos
+When: requiera la información
+Then: debería mostrar el código, nombre, precio, costo, existencia, marca, total en ventas, total en costos y total en utilidad 
+And: debe estar ordenado por marca y nombre de artículo
+*/
+
+select  codigo,nombre,d.precio, d.costoTotal costo, existencia, m.nombreMarca marca,
+d.cantidad*d.precio totalVenta, d.costoTotal*d.cantidad totalCosto,
+(d.precio-d.costoTotal)*d.cantidad totalUtilidad
+from Productos p
+inner join marca m on p.idmarca=m.idmarca
+inner join SalidaDetalle d on p.id=d.idproducto
+order by m.nombreMarca,p.nombre
+
