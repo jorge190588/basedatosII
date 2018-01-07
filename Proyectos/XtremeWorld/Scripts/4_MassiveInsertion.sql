83053fb825cf3ca8a8634c91d713e4bc78d2993b
Daniel Estupe <DanielEstupe@users.noreply.github.com>
2018-01-06 23:42:32 -0600

    Create 4_MassiveInsertion.sql

---
+++ Proyectos/XtremeWorld/Scripts/4_MassiveInsertion.sql
@@ -0,0 +1,23 @@
+use XtremeWorld;
+
+-- INSERTAR FUNCIONES
+execute sp_InsertarFuncionesMasivo
+ 
+-------------------------------------------------------------------------------------------
+
+
+-- INSERTAR REGISTROS DE ENTRADA Y SALIDA
+execute sp_InsertarRegistroIOMasivo 
+	
+
+-------------------------------------------------------------------------------------------
+
+-- INSERTAR REGISTROS DE VENTAS
+
+execute sp_InsertarVentasMasivo
+ 
+------------------------------------------------------------------------------------------
+
+-- INSERTAR REGISTROS DE CALIFICACIONES
+
+execute sp_InsertarCalificacionesMasivo 
