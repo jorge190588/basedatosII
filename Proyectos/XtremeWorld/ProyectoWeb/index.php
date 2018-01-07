1a766c7be78204b102ea3baac5706bd9361e2d64
Daniel Estupe <DanielEstupe@users.noreply.github.com>
2018-01-06 23:50:01 -0600

    Create index.php

---
+++ Proyectos/XtremeWorld/ProyectoWeb/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>XtremeWorld</title>
+    <link rel="shortcut icon" href="style/img/logo.png" />
+    <link type="text/css" rel="stylesheet" href="style/materialize.min.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+    <nav class="blue-grey darken-3">
+        <div class="nav-wrapper">
+          <a class="brand-logo center">XtremeWorld</a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li class="active"><a href="#" >Inicio</a></li>
+          </ul>
+        </div>
+    </nav>
+    <div class="container">
+       <div class="section">
+            <div class="row">
+                <a href="views/empleados.php">
+                <div class="col s12 m6">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Empleados</h5>
+                    </div>
+                </div>
+                </a>
+                <a href="views/horario_laboral.php">
+                <div class="col s12 m6">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Horarios Laborales</h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="row">
+               <a href="views/registro_entrada_salida.php">
+                <div class="col s12 m6">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Registros de Entrada y Salida</h5>
+                    </div>
+                </div>
+                </a>
+                <a href="views/calificaciones.php">
+                <div class="col s12 m6" href="">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Calificaciones</h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="row">
+               <a href="views/facturas.php">
+                <div class="col s12 m6">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Facturas</h5>
+                    </div>
+                </div>
+                </a>
+                <a href="views/venta_productos.php">
+                <div class="col s12 m6" href="">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Venta de Productos</h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="row">
+               <a href="views/funciones.php">
+                <div class="col s12 m6">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Funciones</h5>
+                    </div>
+                </div>
+                </a>
+                <a href="views/producto_servicio.php">
+                <div class="col s12 m6" href="">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Productos y Servicios</h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+            <div class="row">
+               <a href="views/ingreso_personas.php">
+                <div class="col s12 m6">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Ingreso de Personas</h5>
+                    </div>
+                </div>
+                </a>
+                <a href="views/entradas.php">
+                <div class="col s12 m6" href="">
+                    <div class="card-panel hoverable blue-grey lighten-5">
+                        <h5 class="center-align">Entradas</h5>
+                    </div>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="action/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="action/materialize.min.js"></script>
+</body>
+</html>
