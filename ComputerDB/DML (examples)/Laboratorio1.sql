9ba615e9fe031ccd0457bf1915c9aa9d424e2005
Daniel Estupe <DanielEstupe@users.noreply.github.com>
2018-02-23 12:22:47 -0600

    Order data and query 5 added

--- ComputerDB/DML (examples)/Laboratorio1.sql
+++ ComputerDB/DML (examples)/Laboratorio1.sql
@@ -1,11 +1,37 @@
 /*
+2.	Scenario: Reporte de artículos (David Fernandez)
+a.	Given: El dueño del negocio requiere un listado de productos
+b.	When: requiera la información
+c.	Then: debería mostrar el código, nombre, precio, costo, existencia y marca 
+d.	And: debe mostrar el recuento de ventas
+e.	And: debe estar ordenado por marca y nombre de artículo.
+*/
+
+ use ComputerDB
+select 
+	id codigo,
+	nombre,
+	p.precio,
+	costo,
+	existencia,
+	m.nombreMarca, 
+	count(sd.cantidad) as [recuento de ventas] 
+from  
+	productos p, marca m, SalidaDetalle sd
+where p.idMarca in (select idMarca from Marca where p.idMarca=m.idMarca) and
+	sd.idProducto in (select idProducto from SalidaDetalle where p.id=sd.idProducto)
+group by id, nombre,costo, existencia, p.precio, m.nombreMarca
+order by nombre, nombreMarca asc
+
+
+/*
 --ESCENARIO 3
 --3.	Scenario: Reporte de ventas (Allan y David)
 --a.	Given: el due±o de un negocio requiere informacion de las ventas
 --b.	When: requiera la informacion
---c.	Then: deberÃƒÂ­a mostrar la fecha, nit, cliente, total de venta, numero de productos vendidos y 
+--c.	Then: deberia mostrar la fecha, nit, cliente, total de venta, numero de productos vendidos y 
         fecha de ultima venta del ultimo trimestre por cliente
---d.	And: debe estar ordenado por A±o, mes y di­a.
+--d.	And: debe estar ordenado por A±o, mes y dia.
 
 */
 
@@ -31,6 +57,8 @@ inner join SalidaDetalle sd on sd.idProducto = p.id
 group by p.nombre
 order by p.nombre asc
 
+
+
 /*
 5.  Scenario: Reporte detallado de utilidad por marca (Eloina Carrillo)
 Given: el dueño de un negocio requiere conocer la utilidad bruta  por marca
@@ -39,15 +67,47 @@ Then: debería mostrar el año, el nombre la marca, utilidad bruta.
 And: debe estar ordenado por año en forma descendente y por nombre de marca en forma ascendente.
 And: debe estar agrupado por año y nombre de marca.
 */
-select year(s.fecha) as Año,
-       m.nombreMarca as Marca,
-       sum(isnull(costoTotal, 0) - isnull(cantidad, 0) * p.costo) as Utilidad_Bruta
-from Marca m
-inner join Productos p on p.idMarca = m.idMarca
-left join SalidaDetalle sd on sd.idProducto = p.id 
-inner join Salida s on s.idSalida = sd.idSalida
-group by m.nombreMarca, year(s.fecha)
-order by year(s.fecha) desc, Marca asc
+
+select *
+from (
+	select case 
+		 when year(s.fecha) is null  then '--'
+		 when year(s.fecha) is not null then convert(varchar(4), year(s.fecha))
+		end 
+		   as Año,
+		   m.nombreMarca as Marca,
+		   isnull(sum((sd.cantidad * sd.precio) - (sd.cantidad*sd.costo)), 0) as Utilidad_Bruta
+	from Marca m
+	left join Productos p on p.idMarca = m.idMarca
+	left join SalidaDetalle sd on sd.idProducto = p.id 
+	left join Salida s on s.idSalida = sd.idSalida
+	group by m.nombreMarca, year(s.fecha)
+	) sb
+order by sb.Año desc, sb.Marca
+
+
+-- Prueba Unitaria: Marcas registradas=49    &    Marcas seleccionadas=49
+
+-- marcas registradas
+select count(distinct nombreMarca) as N_Marcas from Marca 
+
+-- marcas seleccionadas
+select count(distinct sb.Marca) N_Marcas_Seleccionadas
+from (
+	select case 
+				when year(s.fecha) is null  then '--'
+				when year(s.fecha) is not null then convert(varchar(4), year(s.fecha))
+			end 
+				as Año,
+		   m.nombreMarca as Marca,
+		   isnull(sum((sd.cantidad * sd.precio) - (sd.cantidad*sd.costo)), 0) as Utilidad_Bruta
+	from Marca m
+	left join Productos p on p.idMarca = m.idMarca
+	left join SalidaDetalle sd on sd.idProducto = p.id 
+	left join Salida s on s.idSalida = sd.idSalida
+	group by m.nombreMarca, year(s.fecha)
+	) sb
+
 
 
 /*6.Scenario: Frecuencia de ventas por cliente. (Guillermo Pisqui)
@@ -62,73 +122,6 @@ select c.idCliente, c.nombreCliente, COUNT(s.idSalida)as frecuencia from cliente
 inner join Salida s on s.idCliente = c.idCliente where s.fecha ='2013-02-12'
 group by c.nombreCliente,c.idCliente
 order by frecuencia asc
-
-
-
-/*
-1.	Scenario: Reporte de clientes
-Given: El dueño del negocio requiere conocer a sus clientes
-When: requiera la información
-Then: un reporte debería mostrar el nombre, dirección, teléfono y nit
-And: debe estar ordenado por nombre 
-And: debe estar filtrado por las ventas del primer semestre de cada año.
-And: debe estar filtrado por el día lunes de las ventas.
-*/
-
-select nombreCliente nombre, direccion,telefono,nit,
-s.fecha,datename(dw,s.fecha) nombreDia,day(s.fecha) dia, 
-datepart(qq,s.fecha) trimestre, datename(mm,s.fecha) nombreMes
-from Clientes c
-inner join Salida s on s.idCliente=c.idCliente
---where month(s.fecha) in (1,2,3,4,5,6) 
-where datepart(qq,s.fecha) in (1,2)
-and datename(dw,s.fecha)='Lunes'
-order by nombreCliente
-
-/*
-2.	Scenario: Reporte de artículos
-Given: El dueño del negocio requiere un listado de productos
-When: requiera la información
-Then: debería mostrar el código, nombre, precio, costo, existencia, marca, total en ventas, total en costos y total en utilidad 
-And: debe estar ordenado por marca y nombre de artículo
-*/
-
-select  codigo,nombre,d.precio,d.cantidad, d.costoTotal costo, existencia, m.nombreMarca marca,
-d.cantidad*d.precio totalVenta, d.costoTotal*d.cantidad totalCosto,
-(d.precio-d.costoTotal)*d.cantidad totalUtilidad
-from Productos p
-inner join marca m on p.idmarca=m.idmarca
-inner join SalidaDetalle d on p.id=d.idproducto
-order by m.nombreMarca,p.nombre
-
-select p.codigo,p.nombre,p.descripcion,
-avg(d.cantidad) cantidadPromedio,
-sum(d.cantidad) cantidadSuma,
-min(d.cantidad) cantidadMinima,
-max(d.cantidad) cantidadMaximo,
-sum(d.cantidad*d.precio) totalVenta, sum(d.costoTotal*d.cantidad) totalCosto,
-sum((d.precio-d.costoTotal)*d.cantidad) totalUtilidad
-from Productos p
-inner join SalidaDetalle d on p.id=d.idproducto
-group by  p.codigo,p.nombre,p.descripcion
-
-update SalidaDetalle set costoTotal=(precio*0.6)
-
-
-select p.codigo,p.nombre,s.fecha,
-sum((d.precio-d.costoTotal)*d.cantidad) totalUtilidad
-from Productos p
-inner join SalidaDetalle d on p.id=d.idproducto
-inner join Salida as s on d.idsalida=s.idsalida
-where s.fecha between dateadd(MONTH,-7,getdate()) and GETDATE()
-group by  p.codigo,p.nombre,s.fecha
-order by p.nombre
-
-
-select fecha from salida
-order by fecha desc
-
-
 
 
 /*
@@ -166,7 +159,7 @@ print(@consulta)
 exec(@consulta)
 
 
--- Prueba con aÑo = 2016 y mes = febrero
+-- Prueba con anho = 2016 y mes = febrero
 
 declare @ingresos float, @egresos float, @utilidad_bruta float
 
@@ -183,39 +176,6 @@ select @egresos = sum(sd.cantidad * sd.costo)
 set @utilidad_bruta = @ingresos - @egresos
 
 select @ingresos as Ingreso, @egresos as Egreso, @utilidad_bruta as Utilidad
-
-
-
-
-
------------------------------------------------------------------------------
-no.#8
-
-select *from salida              select fecha from salida
-select *from SalidaDetalle
-select *from Departamento
-select *from Municipio
-select *from Productos
-
-select cantidad from salidadetalle
- select  sum (cantidad) from salidadetalle 
- select sum (existencia) from Productos
-
-select d.nombredepartamento as [nombre] , m.nombremunicipio, salida.idproducto, salida.cantidad , salida.costototal, sa.fecha
-  
-from Departamento d inner join Municipio m
-on d.idDepartamento = m.idMunicipio 
-inner join SalidaDetalle salida
-on d.idDepartamento = salida.idSalidaDetalle
-inner join salida sa
-on d.iddepartamento = sa.idsalida
-
-go
-
------------------------------------------------------------------------------------
-select fecha from Salida
-min (fecha) as fechaminima, max (fecha) as fechamaxima
-------------------------------------------------------------------------------------
 
 
 
@@ -253,28 +213,71 @@ set @consulta = 'select pr.idProveedor, pr.nombreProveedor, pr.direccion, pr.tel
  exec(@consulta)
 
 
-# 2 david fernandez
---2.REPORTE DE ARTICULOS
-
- use ComputerDB
-select 
-	id codigo,
-	nombre,
-	p.precio,
-	costo,
-	existencia,
-	m.nombreMarca, 
-	count(sd.cantidad) as [recuento de ventas] 
-from  
-	productos p, marca m, SalidaDetalle sd
-where p.idMarca in (select idMarca from Marca where p.idMarca=m.idMarca) and
-	sd.idProducto in (select idProducto from SalidaDetalle where p.id=sd.idProducto)
-group by id, nombre,costo, existencia, p.precio, m.nombreMarca
-order by nombre, nombreMarca asc
 
 
-select * from entradadetalle
-select * from entrada
+
+-- Consultas Adicionales  2017
+
+/*
+	Scenario: Reporte de clientes   [Ing. Santos]
+Given: El dueño del negocio requiere conocer a sus clientes
+When: requiera la información
+Then: un reporte debería mostrar el nombre, dirección, teléfono y nit
+And: debe estar ordenado por nombre 
+And: debe estar filtrado por las ventas del primer semestre de cada año.
+And: debe estar filtrado por el día lunes de las ventas.
+*/
+
+select nombreCliente nombre, direccion,telefono,nit,
+s.fecha,datename(dw,s.fecha) nombreDia,day(s.fecha) dia, 
+datepart(qq,s.fecha) trimestre, datename(mm,s.fecha) nombreMes
+from Clientes c
+inner join Salida s on s.idCliente=c.idCliente
+--where month(s.fecha) in (1,2,3,4,5,6) 
+where datepart(qq,s.fecha) in (1,2)
+and datename(dw,s.fecha)='Lunes'
+order by nombreCliente
+
+
+
+/*
+	Scenario: Reporte de artículos   [Ing. Santos]
+Given: El dueño del negocio requiere un listado de productos
+When: requiera la información
+Then: debería mostrar el código, nombre, precio, costo, existencia, marca, total en ventas, total en costos y total en utilidad 
+And: debe estar ordenado por marca y nombre de artículo
+*/
+
+select  codigo,nombre,d.precio,d.cantidad, d.costoTotal costo, existencia, m.nombreMarca marca,
+d.cantidad*d.precio totalVenta, d.costoTotal*d.cantidad totalCosto,
+(d.precio-d.costoTotal)*d.cantidad totalUtilidad
+from Productos p
+inner join marca m on p.idmarca=m.idmarca
+inner join SalidaDetalle d on p.id=d.idproducto
+order by m.nombreMarca,p.nombre
+
+select p.codigo,p.nombre,p.descripcion,
+avg(d.cantidad) cantidadPromedio,
+sum(d.cantidad) cantidadSuma,
+min(d.cantidad) cantidadMinima,
+max(d.cantidad) cantidadMaximo,
+sum(d.cantidad*d.precio) totalVenta, sum(d.costoTotal*d.cantidad) totalCosto,
+sum((d.precio-d.costoTotal)*d.cantidad) totalUtilidad
+from Productos p
+inner join SalidaDetalle d on p.id=d.idproducto
+group by  p.codigo,p.nombre,p.descripcion
+
+
+
+select p.codigo,p.nombre,s.fecha,
+sum((d.precio-d.costoTotal)*d.cantidad) totalUtilidad
+from Productos p
+inner join SalidaDetalle d on p.id=d.idproducto
+inner join Salida as s on d.idsalida=s.idsalida
+where s.fecha between dateadd(MONTH,-7,getdate()) and GETDATE()
+group by  p.codigo,p.nombre,s.fecha
+order by p.nombre
+
 
 
 
