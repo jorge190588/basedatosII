7f8c1af157e6f920ceda54fa6aa81840338d88db
jorgesantosn <netneill@hotmail.com>
2018-03-03 12:38:38 -0600

    update cursor example

--- ComputerDB/TransSql (Examples)/7. Cursor con tabla temporal (Ejemplo).sql
+++ ComputerDB/TransSql (Examples)/7. Cursor con tabla temporal (Ejemplo).sql
@@ -1,4 +1,4 @@
-﻿use BDII
+﻿use ComputerDB;
 --Tabla 
 declare @resultados table(id int, resultado int)
 --insert into @resultados (id,resultado) values ('1','100')
@@ -14,7 +14,7 @@ declare @id int, @nombre varchar(50), @recuentoVenta int
 --1ro. declarar el cursor curEntrega.
 declare curEjemplo cursor for
 --2do. seleccionar los datos base, ya estan agrupados los datos.
-select id,nombre from cliente
+select top 3 idcliente id, nombreCliente nombre from clientes
 
 --3ro. abrir el cursor.
 open curEjemplo
@@ -23,10 +23,11 @@ into @id,@nombre
 while @@fetch_status = 0
 begin
 --inicio cuerpo del cursor
-	select @recuentoVenta = count(*) from VENTAS v, VENTASDETALLE d
-	where v.id=d.id and v.id=@id 
+	select @recuentoVenta = count(*) from salida s, SalidaDetalle d
+	where s.idsalida=d.idsalida and s.idCliente=@id 
 	
-	insert into @resultados (id, resultado) values (@id,@recuentoVenta)
+	insert into @resultados (id, resultado) 
+	values (@id,@recuentoVenta)
 	--update cliente set recuentoVenta=@recuentoVenta where id=@id
 	print 'ventas Promedio '+convert(varchar(50),@recuentoVenta)+' id = '+convert(varchar(50),@id)
 
@@ -39,4 +40,4 @@ end
 close curEjemplo
 deallocate curEjemplo
 
-select * from @resultados
+
