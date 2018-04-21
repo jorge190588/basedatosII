a2ef6bf55142a6b7544757a4b9163f38f55d4bbb
jorgesantosn <netneill@hotmail.com>
2018-04-21 11:45:48 -0600

    update error file

--- docker/files/ComputerDB/TransSql_Examples/18. errors.sql
+++ docker/files/ComputerDB/TransSql_Examples/18. errors.sql
@@ -1,15 +1,34 @@
 --https://docs.microsoft.com/en-us/sql/t-sql/functions/error-message-transact-sql?view=sql-server-2017
 --https://www.red-gate.com/simple-talk/sql/database-administration/handling-errors-in-sql-server-2012/
-
+-- first example
 BEGIN TRY  
     -- Generate a divide-by-zero error.  
     SELECT 1/0;  
 END TRY  
 BEGIN CATCH  
-    SELECT ERROR_MESSAGE() AS ErrorMessage;  
+    SELECT ERROR_MESSAGE() as message	
 END CATCH;  
-GO  
 
+
+-- second example
+BEGIN TRY  
+    -- Generate a divide-by-zero error.  
+    SELECT 1/0;  
+END TRY  
+BEGIN CATCH  
+	declare @mensaje varchar(50)
+    SELECT @mensaje=ERROR_MESSAGE()
+	insert into errores(nombre) values(@mensaje)
+END CATCH;  
+GO
+
+select * from errores
+
+create table errores (id int primary key not null identity, 
+nombre varchar(50), 
+fecha datetime default getdate())
+  
+-- third example
 BEGIN TRY  
     -- Generate a divide-by-zero error.  
     SELECT 1/0;  
