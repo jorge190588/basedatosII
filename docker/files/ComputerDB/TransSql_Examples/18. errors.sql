21adb06bbe4e8c200c1a597982a36fb3fdeb8fcd
Jorge Santos <netneill@hotmail.com>
2020-04-18 17:47:43 -0600

    update errors.sql file

--- docker/files/ComputerDB/TransSql_Examples/18. errors.sql
+++ docker/files/ComputerDB/TransSql_Examples/18. errors.sql
@@ -4,6 +4,7 @@
 BEGIN TRY  
     -- Generate a divide-by-zero error.  
     SELECT 1/0;  
+	select 1
 END TRY  
 BEGIN CATCH  
     SELECT ERROR_MESSAGE() as message	
@@ -23,7 +24,7 @@ END CATCH;
 GO
 
 select * from errores
-
+drop table errores
 create table errores (id int primary key not null identity, 
 nombre varchar(50), 
 fecha datetime default getdate())
@@ -34,7 +35,8 @@ BEGIN TRY
     SELECT 1/0;  
 END TRY  
 BEGIN CATCH  
-    SELECT  
+    insert into errores (ErrorNumber,ErrorSeverity,ErrorState,ErrorProcedure,ErrorLine,ErrorMessage)
+	SELECT  
         ERROR_NUMBER() AS ErrorNumber  
         ,ERROR_SEVERITY() AS ErrorSeverity  
         ,ERROR_STATE() AS ErrorState  
@@ -44,4 +46,44 @@ BEGIN CATCH
 END CATCH;  
 GO  
 
- 
+create table errores (id int primary key not null identity, 
+ErrorNumber int,
+ErrorSeverity int,
+ErrorState int,
+ErrorProcedure varchar(500), 
+ErrorLine int,
+ErrorMessage varchar(500),
+fecha datetime default getdate())
+
+create table clientes (id int primary key not null identity, 
+nombre varchar(5), 
+fecha datetime default getdate())
+select * from clientes
+
+
+--fourth
+declare @result int, @errorMessage varchar(500)
+
+BEGIN TRY  
+	insert into clientes (nombre) values ('jorge');
+	--insert into clientes (nombre) values ('salvador');
+	insert into errores(id) values (1);
+	select @result=1;
+END TRY  
+BEGIN CATCH  
+	insert into errores (ErrorNumber,ErrorSeverity,ErrorState,ErrorProcedure,ErrorLine,ErrorMessage)
+	SELECT  
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+	select @result=0;
+	select @errorMessage= case when ERROR_NUMBER()=8152 then 'El texto de un campo es mayor a su longitud' else 
+							case when ERROR_NUMBER()=544 then 'División entre zero no  permitida' else 
+							'Error en la operacion' end end ;
+END CATCH;  
+
+select @result result,@errorMessage errorMessage
+GO  
