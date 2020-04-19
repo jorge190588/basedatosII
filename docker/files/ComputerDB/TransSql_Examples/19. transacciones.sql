28bd42eae638e3cca93597d61ffda4968d1d899d
Jorge Santos <netneill@hotmail.com>
2020-04-18 18:02:28 -0600

    update 19. transacciones.sql file

--- docker/files/ComputerDB/TransSql_Examples/19. transacciones.sql
+++ docker/files/ComputerDB/TransSql_Examples/19. transacciones.sql
@@ -42,3 +42,60 @@ END CATCH
 
 
  
+ BEGIN (OBLIGATORIO)
+ COMMIT O ROLLBACK
+
+
+
+
+
+ -- Ejemplo 3
+ 
+create table errores (id int primary key not null identity, 
+ErrorNumber int,
+ErrorSeverity int,
+ErrorState int,
+ErrorProcedure varchar(500), 
+ErrorLine int,
+ErrorMessage varchar(500),
+fecha datetime default getdate())
+
+create table clientes (id int primary key not null identity, 
+nombre varchar(5), 
+fecha datetime default getdate())
+select * from clientes
+
+delete from clientes
+select * from clientes
+--TRANSACCION
+declare @result int, @errorMessage varchar(500)
+
+BEGIN TRY  
+	BEGIN TRANSACTION
+		insert into clientes (nombre) values ('jorge');
+		--insert into clientes (nombre) values ('salvador');
+		select 0/0;
+		--insert into errores(id) values (1);
+		select @result=1;
+	commit transaction;
+END TRY  
+BEGIN CATCH  
+	rollback transaction;
+	insert into errores (ErrorNumber,ErrorSeverity,ErrorState,ErrorProcedure,ErrorLine,ErrorMessage)
+	SELECT  
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+	select @result=0;
+	select @errorMessage= case when ERROR_NUMBER()=8152 then 'El texto de un campo es mayor a su longitud' else 
+							case when ERROR_NUMBER()=544 then 'No se pudo generar el correlativo' else 
+							case when ERROR_NUMBER()=8134 then 'División entre zero no  permitida' else 
+							'Error en la operacion' end end end;
+END CATCH;  
+
+select @result result,@errorMessage errorMessage;
+ 
+select * from errores
